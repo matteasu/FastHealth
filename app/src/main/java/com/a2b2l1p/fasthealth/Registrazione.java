58d1a150896fe1a342feefbf42b11285d85be2bf
Matteo <asunimatteo@gmail.com>
2021-06-08 11:58:07 +0200

    Schermata registrazione senza logica

---
+++ app/src/main/java/com/a2b2l1p/fasthealth/Registrazione.java
@@ -0,0 +1,14 @@
+package com.a2b2l1p.fasthealth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Registrazione extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registrazione);
+    }
+}
